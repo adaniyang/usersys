ebbd60b3e6c226bba3200d807a33ad1735cbda37
adaYang <993112135@qq.com>
2018-11-19 16:05:07 +0800

    修改swagger访问不到的问题

--- src/main/java/com/springboot/SwaggerConfig.java
+++ src/main/java/com/springboot/SwaggerConfig.java
@@ -1,16 +1,15 @@
 package com.springboot;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@Configuration //因为把springBoot启动类的注释写到这里 所以注释了这个
-@EnableSwagger2
+@Configuration
 public class SwaggerConfig {
 
     @Bean
@@ -28,7 +27,7 @@ public class SwaggerConfig {
         return new ApiInfoBuilder()
                 .title("springboot利用swagger构建api文档")
                 .description("简单优雅的restfun风格")
-                .termsOfServiceUrl("http://blog.csdn.net/saytime")
+                .termsOfServiceUrl("http://blog.csdn.net/saytime") //http://localhost:8080/swagger-ui.html
                 .version("1.0")
                 .build();
     }
--- src/main/java/com/springboot/UsersysApplication.java
+++ src/main/java/com/springboot/UsersysApplication.java
@@ -2,11 +2,12 @@ package com.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 //@ComponentScan(basePackages = {"com.springboot.activeMQ"})
 //@MapperScan(value = "com.springboot.usersys.dao")
-//@EnableSwagger2 //将这个注释写到SwaggerConfig上了
+@EnableSwagger2
 public class UsersysApplication {
 
 	public static void main(String[] args) {
